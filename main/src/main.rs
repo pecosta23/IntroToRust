91f6b7fafca104970a9291541174ecc6b33b0033
Pedro Costa <pedrocosta2802@gmail.com>
2025-12-10 21:16:20 -0300

    Add: let and let mut

--- main/src/main.rs
+++ main/src/main.rs
@@ -2,6 +2,16 @@ use reqwest;
 use tokio;
 
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Result<(), Box<dyn std::error::Error>>{
+    //hello
+    println!("Hello, world!\n");
+
+    //imutable and mutable
+    let x = 10;
+    let mut y = 3;
+    println!("x = {}, y = {}\n", x, y);
+    y = 5;
+    println!("y = {}\n", y);
+
+    Ok(())
 }
