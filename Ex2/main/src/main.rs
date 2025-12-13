8ed09304ffb08397b3d7948c66db0d8383904c52
Pedro Costa <pedrocosta2802@gmail.com>
2025-12-13 20:23:44 -0300

    Simple math exercise

--- Ex2/main/src/main.rs
+++ Ex2/main/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+// std is to read user input
+use std::io;
+
+fn main(){
+    println!("Type any whole number\n");
+    //num 1
+    let mut a = String::new();
+    io::stdin().read_line(&mut a).unwrap();
+    let a: i32 = a.trim().parse().unwrap();
+
+    println!("Please type any of these operators: + - * /\n");
+    //operator
+    let mut ope = String::new();
+    io::stdin().read_line(&mut ope).unwrap();
+    let ope: char = ope.trim().parse().unwrap();
+
+    println!("Type other whole number\n");
+    //num 2
+    let mut b = String::new();
+    io::stdin().read_line(&mut b).unwrap();
+    let b: i32 = b.trim().parse().unwrap();
+
+    let result = match ope {
+        '+' => a + b,
+        '-' => a - b,
+        '*' => a * b,
+        '/' => a / b,
+        _ => {
+            println!("Invalid operator\n");
+            0
+        }
+    };
+
+    println!("Result: {}", result)
 }
+
